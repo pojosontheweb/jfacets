95d036ff5a19004c79ab4f7d03f4b47413252a29
vankeisb <vankeisb@ce437dc7-d02c-0410-bf88-fdf4ecd7fe3d>
2007-05-29 17:38:49 +0000

    added getFacetClass to GroovyFacetDescriptor increased version number
    git-svn-id: https://svn.code.sf.net/p/jfacets/svn/trunk@169 ce437dc7-d02c-0410-bf88-fdf4ecd7fe3d

--- src/main/java/net/sourceforge/jfacets/groovy/GroovyFacetDescriptor.java
+++ src/main/java/net/sourceforge/jfacets/groovy/GroovyFacetDescriptor.java
@@ -1,6 +1,12 @@
 package net.sourceforge.jfacets.groovy;
 
+import groovy.lang.GroovyClassLoader;
+
 import java.io.File;
+import java.io.IOException;
+
+import org.apache.log4j.Logger;
+import org.codehaus.groovy.control.CompilationFailedException;
 
 import net.sourceforge.jfacets.FacetDescriptor;
 
@@ -10,6 +16,11 @@ import net.sourceforge.jfacets.FacetDescriptor;
  * that points to the Groovy Script file.
  */
 public class GroovyFacetDescriptor extends FacetDescriptor {
+	
+	private static final Logger logger = Logger.getLogger(GroovyFacetDescriptor.class);
+	
+	private static GroovyClassLoader groovyClassLoader = new GroovyClassLoader(); 
+	private Class facetClass;
 
 	/** the groovy script file for this descriptor */
 	private File scriptFile;
@@ -24,6 +35,24 @@ public class GroovyFacetDescriptor extends FacetDescriptor {
 		this.scriptFile = scriptFile;
 	}
 	
+	
+	@Override
+	public Class getFacetClass() {
+		if (facetClass==null) {
+			if (logger.isDebugEnabled()) {
+				logger.debug("Class not yet parsed, parsing from file " + scriptFile.getAbsolutePath() + ", I am " + this);
+			}
+			try {
+				facetClass = groovyClassLoader.parseClass(scriptFile);
+			} catch (CompilationFailedException e) {
+				logger.warn("Compilation failed, file=" + scriptFile.getAbsolutePath() + ", I am " + this, e);
+			} catch (IOException e) {
+				logger.warn("Caught an IOException while compiling from file " + scriptFile.getAbsolutePath());
+			}
+		}
+		return facetClass;
+	}
+
 	public String toString() {
 		return "{GroovyFacetDescriptor name=" + getName() + ", profileId=" + getProfileId() + ", targetObjectType=" + getTargetObjectType() + ", scriptFile=" + scriptFile.getAbsolutePath() + "]";
 	}
